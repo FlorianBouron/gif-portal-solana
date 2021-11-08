9fdde78e7f991829296fe9822f2d5583dbb139bf
Florian Bouron <bouron.florian@yahoo.fr>
2021-11-08 07:54:09 +0100

    Add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header">🖼 GIF Portal</p>
+          <p className="sub-text">
+            View your GIF collection in the metaverse ✨
+          </p>
+        </div>
+        <div className="footer-container">
+          <div className="footer-text">
+            Made with (L) by Florian
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
